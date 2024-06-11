3d9d5773b634b8b904e3e7d986b8aac16284de10
Ioanna-arvaniti <72710833+Ioanna-arvaniti@users.noreply.github.com>
2024-06-11 21:49:02 +0300

    Add files via upload
    1oς Κώδικας:
    Προστέθηκε η βιβλιοθήκη <unordered_map>: Προστέθηκε η απαραίτητη βιβλιοθήκη για τη χρήση του αντικειμένου unordered_map.
    Ελέγχος Εγκυρότητας Μεγέθους Αραιού Πίνακα: Προστέθηκε έλεγχος για την εγκυρότητα του μεγέθους του αραιού πίνακα, ώστε να μην υπερβαίνει τον αριθμό των στοιχείων στον πίνακα.
    Σχόλια στον Κώδικα: Προστέθηκαν σχόλια στον κώδικα για καλύτερη κατανόηση της λειτουργίας του και του τι κάνει κάθε τμήμα.
    2ος Κώδικας
    Ελέγχος Εγκυρότητας Αραιού Πίνακα: Προστέθηκε ένας έλεγχος για την εγκυρότητα του αραιού πίνακα, ώστε να μην υπερβαίνει τον αριθμό των μη μηδενικών στοιχείων του πίνακα.
    Έξοδος Σφάλματος: Εάν το μέγεθος του αραιού πίνακα δεν είναι έγκυρο, τότε εμφανίζεται ένα μήνυμα σφάλματος και ο κώδικας τερματίζει με κωδικό σφάλματος 1.
    Σχόλια στον Κώδικα: Προστέθηκαν σχόλια στον κώδικα για καλύτερη κατανόηση της λειτουργίας του και του τι κάνει κάθε τμήμα.
    3ος Κώδικας:
    Ελέγχος Εγκυρότητας Μεγέθους Αραιού Πίνακα: Προστέθηκε έλεγχος για την εγκυρότητα του μεγέθους του αραιού πίνακα, ώστε να μην υπερβαίνει τον αριθμό των μη μηδενικών στοιχείων του πίνακα.
    Έξοδος Σφάλματος: Εάν το μέγεθος του αραιού πίνακα δεν είναι έγκυρο, τότε εμφανίζεται ένα μήνυμα σφάλματος.
    Σχόλια στον Κώδικα: Προστέθηκαν σχόλια στον κώδικα για καλύτερη κατανόηση της λειτουργίας του και του τι κάνει κάθε τμήμα.

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,51 +1,41 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <vector>
+#include <unordered_map> // Προσθέσαμε την απαραίτητη βιβλιοθήκη για το std::unordered_map
 
 using namespace std;
 
-/*
-    Two Sum will take a n array of numbers and a target as input
-    and returns the pair of indicies of array which add up to that target
-
-    if such pair doesn't exist, it will simply return an empty array
-*/
 vector<int> twoSum(vector<int>& nums, int target) {
-    // a map to store value and it's corresponding index
-    unordered_map<int, int> mp;
+    unordered_map<int, int> mp; // Χρησιμοποιούμε std::unordered_map για να αποθηκεύσουμε τα νούμερα και τα αντίστοιχα δείκτες τους
+    vector<int> result;
 
-    // a vector to store final result
-    vector<int> x;
-
-    // iterating through the array
+    // Προσπελαύνουμε το vector nums
     for (int i = 0; i < nums.size(); i++) {
-        // check if map contains the target - curr_value
-        // if yes we found the pair
-        // else add it to map for referencing further in the array
-        if (mp.find(target-nums[i]) != mp.end()) {
-            x.push_back(i);
-            x.push_back(mp[target-nums[i]]);
-            break;
+        // Ελέγχουμε αν υπάρχει ήδη το target - nums[i] στον χάρτη
+        // Αν ναι, βρήκαμε το ζευγάρι που μας ενδιαφέρει
+        // Αλλιώς, προσθέτουμε τον αριθμό και τον δείκτη του στον χάρτη για περαιτέρω αναφορά
+        if (mp.find(target - nums[i]) != mp.end()) {
+            result.push_back(mp[target - nums[i]]); // Προσθέτουμε τον δείκτη που βρήκαμε στο result
+            result.push_back(i); // Προσθέτουμε τον τρέχοντα δείκτη i στο result
+            break; // Βγαίνουμε από την επανάληψη, αφού βρήκαμε το ζευγάρι
         } else {
-            mp[nums[i]] = i;
+            mp[nums[i]] = i; // Προσθέτουμε τον αριθμό και τον δείκτη του στον χάρτη
         }
     }
         
-    return x;
+    return result;
 }
 
-
-/*
-    Driver code
-*/
 int main() {
-
-    vector<int> arr;
-    arr.push_back(1);
-    arr.push_back(4);
-    arr.push_back(6);
-    arr.push_back(3);
-    arr.push_back(8);
+    vector<int> arr = {1, 4, 6, 3, 8}; // Μπορούμε να αρχικοποιήσουμε το vector με αυτό τον τρόπο
 
     vector<int> res = twoSum(arr, 10);
-    cout<<arr[res[0]]<<" "<<arr[res[1]]<<endl;
+    
+    // Ελέγχουμε αν υπάρχουν στοιχεία στο result
+    if (res.size() == 2) {
+        cout << arr[res[0]] << " " << arr[res[1]] << endl; // Εκτυπώνουμε τα στοιχεία που αντιστοιχούν στους δείκτες που βρήκαμε
+    } else {
+        cout << "No two sum solution found." << endl; // Αν το result δεν έχει μέγεθος 2, τότε δεν βρέθηκε λύση
+    }
+
+    return 0;
 }
